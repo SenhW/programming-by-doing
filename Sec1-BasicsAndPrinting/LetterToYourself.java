db491a2c8bb800c35b781c5b48a32d3cc999255e
SenhW <senhwang3@gmail.com>
2014-12-25 15:08:24 -0500

    letter to yourself

---
+++ Sec1-BasicsAndPrinting/LetterToYourself.java
@@ -0,0 +1,23 @@
+/**
+ * A Letter to Yourself
+ * http://programmingbydoing.com/a/letter-to-yourself.html
+ * Write a program that displays your name and address on the screen as if it were a letter. Your output should look
+ * something like that below.
+ */
+
+public class LetterToYourself {
+
+    public static void main(String[] args) {
+        System.out.println("+---------------------------------------------------------+");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                         |");
+        System.out.println("|                                                         |");
+        System.out.println("|                              Bill Gates                 |");
+        System.out.println("|                              1 Microsoft Way            |");
+        System.out.println("|                              Redmond, WA 98104          |");
+        System.out.println("|                                                         |");
+        System.out.println("+---------------------------------------------------------+");
+    }
+}
