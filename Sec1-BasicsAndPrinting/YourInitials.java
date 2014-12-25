2ac7c6f9256b12f51f4b13e434b04ceb9e25b327
SenhW <senhwang3@gmail.com>
2014-12-25 15:22:23 -0500

    your initials

---
+++ Sec1-BasicsAndPrinting/YourInitials.java
@@ -0,0 +1,18 @@
+/**
+ * Your Initials
+ * http://programmingbydoing.com/a/your-initials.html
+ * Display your initials on the screen in block letters as shown.
+ */
+
+public class YourInitials {
+
+    public static void main(String[] args) {
+        System.out.println(" SSS   L      W   W");
+        System.out.println("S   S  L      W   W");
+        System.out.println("S      L      W   W");
+        System.out.println(" SSS   L      W W W");
+        System.out.println("    S  L      WW WW");
+        System.out.println("S   S  L      WW WW");
+        System.out.println(" SSS   LLLLL  W   W");
+    }
+}
