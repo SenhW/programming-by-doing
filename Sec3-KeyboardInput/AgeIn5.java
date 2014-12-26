ade40bfb8b92d656ccc4c4d2e3938fd9cd562438
SenhW <senhwang3@gmail.com>
2014-12-25 20:36:38 -0500

    age in 5 years

---
+++ Sec3-KeyboardInput/AgeIn5.java
@@ -0,0 +1,25 @@
+/***
+ * Age in Five Years
+ * http://programmingbydoing.com/a/age-in-five-years.html
+ * Ask the user for their name. Then display their name to prove that you can recall it. Ask them for their age. Then
+ * display what their age would be five years from now. Then display what their age would be five years ago.
+ */
+
+import java.util.Scanner;
+
+public class AgeIn5 {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        String name;
+        int age;
+        System.out.print("Hello. What is your name? ");
+        name = keyboard.next();
+        System.out.println();
+        System.out.print("Hi, " + name + "! How old are you? ");
+        age = keyboard.nextInt();
+        System.out.println();
+        System.out.println("Did you know that in five years you will be " + (age + 5) + " years old?");
+        System.out.println("And five years ago you were " + (age - 5) + "! Imagine that!");
+    }
+}
